493f2e279a7b46c25b654f0c42f28659e932512b
Victor Kurinny <tyokman@gmail.com>
2019-11-22 17:51:33 +0200

    Добавлен фреймвор SpaceView

---
+++ SpaceView/SpaceView.h
@@ -0,0 +1,19 @@
+//
+//  SpaceView.h
+//  SpaceView
+//
+//  Created by Victor Kurinny on 11/22/19.
+//  Copyright © 2019 horoko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpaceView.
+FOUNDATION_EXPORT double SpaceViewVersionNumber;
+
+//! Project version string for SpaceView.
+FOUNDATION_EXPORT const unsigned char SpaceViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpaceView/PublicHeader.h>
+
+
