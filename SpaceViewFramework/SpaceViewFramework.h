245149c47794632c85d61605d5d585468a26d909
Victor Kurinny <tyokman@gmail.com>
2019-11-22 18:00:29 +0200

    SpaceViewFramework вынесен в отдельный проект

---
+++ SpaceViewFramework/SpaceViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  SpaceViewFramework.h
+//  SpaceViewFramework
+//
+//  Created by Victor Kurinny on 11/22/19.
+//  Copyright © 2019 BeatHead. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SpaceViewFramework.
+FOUNDATION_EXPORT double SpaceViewFrameworkVersionNumber;
+
+//! Project version string for SpaceViewFramework.
+FOUNDATION_EXPORT const unsigned char SpaceViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpaceViewFramework/PublicHeader.h>
+
+
